dbd7b127e106a4797f6decb6b5c8fd1b2a749932
Ryill <pchapron74@gmail.com>
2024-03-26 13:26:13 -0400

    Test Kinect rotation

--- JeuStarWars/Assets/Scripts/ControlleurKinect.cs
+++ JeuStarWars/Assets/Scripts/ControlleurKinect.cs
@@ -20,6 +20,9 @@ public class Script : MonoBehaviour
         KinectInterop.JointType jointdroite = KinectInterop.JointType.HandtipRight;
         KinectInterop.JointType jointgauche = KinectInterop.JointType.HandtipLeft;
 
+        KinectInterop.JointType jointPouceDroit = KinectInterop.JointType.ThumbRight;
+        KinectInterop.JointType jointPoignetDroit = KinectInterop.JointType.WristRight;
+
 
         KinectManager kinectManager = KinectManager.Instance;
         if (kinectManager && kinectManager.IsInitialized())
@@ -28,22 +31,34 @@ public class Script : MonoBehaviour
             {
                 ulong userId = kinectManager.GetUserIdByIndex(0);
 
-                if (kinectManager.IsJointTracked(userId, jointdroite))
+                if (kinectManager.IsJointTracked(userId, jointdroite) && kinectManager.IsJointTracked(userId, jointPouceDroit) && kinectManager.IsJointTracked(userId, jointPoignetDroit))
                 {
                     Vector3 jointdroitePos = kinectManager.GetJointPosition(userId, jointdroite);
                     Debug.Log("La position de la main droite est " + jointdroitePos);
                     cubed.transform.position = new Vector3(jointdroitePos.x * 15, jointdroitePos.y * 5);
                     cubed.transform.position -= new Vector3(0, 6.5f, 0);
-                    // do something with the joint position
+
+                    Vector3 jointPouceDroitPos = kinectManager.GetJointPosition(userId, jointPouceDroit);
+                    Vector3 jointPoignetDroitPos = kinectManager.GetJointPosition(userId, jointPoignetDroit);
+                    
+                    cubed.transform.LookAt(jointPouceDroitPos);
+
+                    
                 }
 
                 if (kinectManager.IsJointTracked(userId, jointgauche))
                 {
                     Vector3 jointgauchePos = kinectManager.GetJointPosition(userId, jointgauche);
+                    
                     Debug.Log("La position de la main droite est " + jointgauchePos);
                     cubeg.transform.position = new Vector3(jointgauchePos.x * 15, jointgauchePos.y * 5);
                     cubeg.transform.position -= new Vector3(0, 6.5f, 0);
-                    // do something with the joint position
+
+                    
+
+
+
+
                 }
             }
         }
